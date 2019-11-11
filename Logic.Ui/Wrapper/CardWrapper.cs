5db28abcc1656e17afb4382e7d39646571c78fa8
Schreiber <jusc7604@INF.FH-FLENSBURG.DE>
2019-11-11 10:39:07 +0100

    Implemented Wrapper Classes

---
+++ Logic.Ui/Wrapper/CardWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De.HsFlensburg.LernkartenApp060.Logic.Ui.Wrapper
+{
+    class CardWrapper
+    {
+        //Dies ist ein Test
+    }
+}
